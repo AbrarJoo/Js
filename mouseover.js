1845deec6bb0745732473942af3ef438bcf748f7
AbrarJoo <log42241@gmail.com>
2026-09-26 13:11:49 +0530

    events

---
+++ mouseover.js
@@ -0,0 +1,23 @@
+//--- mouseover and mouseout
+// let abcd = document.querySelector("#abcd");
+
+// abcd.addEventListener("mouseover", function () {
+//   abcd.style.backgroundColor = "yellow";
+// });
+// abcd.addEventListener("mouseout", function () {
+//   abcd.style.backgroundColor = "red";
+// });
+
+//--mousemove
+// let abcd = document.querySelector("#abcd");
+
+// window.addEventListener("mousemove", function (details) {
+//   abcd.style.top = details.clientY + "px";
+//   abcd.style.left = details.clientX + "px";
+//   //   console.log(details.clientX,details.clientY);'
+// });
+
+
+
+//event object, event target,event type,prevent deafault
+
